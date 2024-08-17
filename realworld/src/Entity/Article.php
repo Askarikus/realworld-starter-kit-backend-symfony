fe094b087184d27f415979968783f646ca19feaf
Askar <askarikus@yandex.ru>
2024-08-17 20:32:56 +0200

    new field in article - body

--- realworld/src/Entity/Article.php
+++ realworld/src/Entity/Article.php
@@ -21,8 +21,61 @@ class Article extends AbstractEntity
     #[ORM\Column(type: 'string', unique: false)]
     private string $description;
 
+    #[ORM\Column(type: 'string', unique: false)]
+    private string $body;
+
     #[ORM\ManyToOne(inversedBy: 'articles', targetEntity: User::class)]
     private User $author;
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
 
     public function getJson(): array
     {
