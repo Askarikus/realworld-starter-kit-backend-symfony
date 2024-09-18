109b32a4c7fe541755ab6563411d2ec81ca81893
Askar <askarikus@yandex.ru>
2024-09-18 16:39:28 +0200

    logout

---
+++ realworld/src/Controller/User/LogoutUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\User;
+
+use App\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LogoutUserController extends BaseController
+{
+    #[Route(path: 'users/logout', name: 'users_logout', methods: ['POST'])]
+    public function __invoke(): Response
+    {
+        return new JsonResponse(
+            data: ['message' => 'Successfully logged out.'],
+            status: Response::HTTP_OK
+        );
+    }
+}
